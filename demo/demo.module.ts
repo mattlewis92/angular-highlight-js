c503550de84c201dc0cc028dcc6a6b3654095112
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-07-23 12:11:45 +0100

    refactor(demo): use es6 imports over require

--- demo/demo.module.ts
+++ demo/demo.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import 'highlight.js/styles/github.css';
+import * as hljs from 'highlight.js/lib/highlight';
+import * as hljsTypescript from 'highlight.js/lib/languages/typescript';
 import { HighlightJsModule } from '../src';
 import { DemoComponent } from './demo.component';
 import { HIGHLIGHT_JS } from '../src/highlightJs.token';
 
 export function highlightJsFactory(): any {
   // only register the typescript language
-  const hljs: any = require('highlight.js/lib/highlight');
-  hljs.registerLanguage('typescript', require('highlight.js/lib/languages/typescript'));
+  hljs.registerLanguage('typescript', hljsTypescript);
   return hljs;
 }
 
